d436300da793936607b14206c520d115560c1124
Pikkiezz <epiphanyvare@gmail.com>
2025-04-09 02:10:56 +0700

    CamelCase fix

--- src/app/svg/loading.js
+++ src/app/svg/loading.js
@@ -9,20 +9,20 @@ export default function Loading() {
         fy=".3125"
         gradientTransform="scale(1.5)"
       >
-        <stop offset="0" stop-color="#373A70"></stop>
-        <stop offset=".3" stop-color="#373A70" stop-opacity=".9"></stop>
-        <stop offset=".6" stop-color="#373A70" stop-opacity=".6"></stop>
-        <stop offset=".8" stop-color="#373A70" stop-opacity=".3"></stop>
-        <stop offset="1" stop-color="#373A70" stop-opacity="0"></stop>
+        <stop offset="0" stopColor="#373A70"></stop>
+        <stop offset=".3" stopColor="#373A70" stopOpacity=".9"></stop>
+        <stop offset=".6" stopColor="#373A70" stopOpacity=".6"></stop>
+        <stop offset=".8" stopColor="#373A70" stopOpacity=".3"></stop>
+        <stop offset="1" stopColor="#373A70" stopOpacity="0"></stop>
       </radialGradient>
       <circle
-        transform-origin="center"
+        transformOrigin="center"
         fill="none"
         stroke="url(#a12)"
-        stroke-width="50"
-        stroke-linecap="round"
-        stroke-dasharray="200 1000"
-        stroke-dashoffset="0"
+        strokeWidth="50"
+        strokeLinecap="round"
+        strokeDasharray="200 1000"
+        strokeDashoffset="0"
         cx="100"
         cy="100"
         r="70"
@@ -39,12 +39,12 @@ export default function Loading() {
         ></animateTransform>
       </circle>
       <circle
-        transform-origin="center"
+        transformOrigin="center"
         fill="none"
         opacity=".2"
         stroke="#373A70"
-        stroke-width="50"
-        stroke-linecap="round"
+        strokeWidth="50"
+        strokeLinecap="round"
         cx="100"
         cy="100"
         r="70"
